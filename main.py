a92eacc563e566858487fe3123fd729ee00e19c5
reiichii <passion.engk@gmail.com>
2020-01-18 21:52:14 +0900

    :zap: Use multi process

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 import json
 import pathlib
+from multiprocessing import Process
 
 def export_file(filename, text):
     dir = 'output'
     file = '{0}/{1}.log'.format(dir, filename)
-    with open(file, 'w') as f:
-        f.write('{}\n'.format(text))
+    with open(file, 'a') as f:
+        f.write(text)
 
 def export_error_file(error_msg, line):
     line = '{0}: {1}'.format(error_msg, line)
@@ -31,25 +33,38 @@ def validate_appli_id(appli_id):
 
     return error_msg
 
+def multi_process(idx, num_process, lines):
+    for line in lines[idx::num_process]:
+        if not line: return
+
+        appli_id, error = get_appli_id(line)
+        if error:
+            export_error_file(error, line)
+            continue
+
+        error_msg = validate_appli_id(appli_id)
+        if len(error_msg) > 0:
+            export_error_file(error_msg, line)
+            continue
+
+        # アプリIDごとにファイルに出力
+        export_file(appli_id, line)
+
 def main():
+    num_process = 8
 
-#    with open('input/events.2019-08-04-03.300000.last.txt') as f:
-    with open('input/test_events.txt') as f:
-        for line in f:
-            if not line: return
+    with open('input/events.2019-08-04-03.300000.last.txt') as f:
+#    with open('input/test_events_1000.txt') as f:
+        lines = f.readlines()
+        process_list = []
+        for i in range(num_process):
+            p = Process(target=multi_process, args=(i, num_process, lines))
+            process_list.append(p)
 
-            appli_id, error = get_appli_id(line)
-            if error:
-                export_error_file(error, line)
-                continue
-
-            error_msg = validate_appli_id(appli_id)
-            if len(error_msg) > 0:
-                export_error_file(error_msg, line)
-                continue
-
-            # アプリIDごとにファイルに出力
-            export_file(appli_id, line)
+        for p in process_list:
+            p.start()
+        for p in process_list:
+            p.join()
 
 if __name__=='__main__':
     main()
